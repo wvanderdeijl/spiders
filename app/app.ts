f3190f1ba5bf6d458e7fbd70d6dbafc899a2a286
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2016-01-17 19:35:24 +0100

    initial blank version created with 'ionic start'
    used --v2 and --ts command line options to use Ionic version 2 and typescript

---
+++ app/app.ts
@@ -0,0 +1,18 @@
+import {App, Platform, Config} from 'ionic-framework/ionic';
+import {TabsPage} from './pages/tabs/tabs';
+
+@App({
+  template: '<ion-nav id="nav" [root]="root" #content></ion-nav>'
+  // Check out the config API docs for more info
+  // http://ionicframework.com/docs/v2/api/config/Config/
+  config: {}
+})
+export class MyApp {
+  constructor(platform: Platform) {
+    this.root = TabsPage;
+
+    platform.ready().then(() => {
+      // Do any necessary cordova or native calls here now that the platform is ready
+    });
+  }
+}
