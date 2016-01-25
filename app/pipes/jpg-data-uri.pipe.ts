ffc673b6ff2e5915ccfef18437f36464ed5f3c28
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2016-01-25 23:41:03 +0100

    jpg-data-uri pipe does not have arguments

--- app/pipes/jpg-data-uri.pipe.ts
+++ app/pipes/jpg-data-uri.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from 'angular2/core';
 
 @Pipe({ name: 'jpgDataUri' })
 export default class JpgDataUriPipe implements PipeTransform {
-    transform(base64: string, args: string[]): string {
+    transform(base64: string): string {
         return base64 && `data:image/jpeg;base64,${base64}`;
     }
 }
